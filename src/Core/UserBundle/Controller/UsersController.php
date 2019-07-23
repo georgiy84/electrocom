e14da94fb8caf519a14339b8cc1b964378b0451c
Georgiy Strashko <georgiys210@gmail.com>
2019-07-23 10:33:08 -0300

    Correccion del error del form (estaba puesto mal el router)

--- src/Core/UserBundle/Controller/UsersController.php
+++ src/Core/UserBundle/Controller/UsersController.php
@@ -4,11 +4,13 @@ namespace Core\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Core\UserBundle\Entity\users;
 
 class UsersController extends Controller {
 
-    public function registerAction(Request $request) {
+    public function registerAction(Request $form) {
         // creates a task and gives it some dummy data for this example
         $users = new users();
         //$users->setTask('Write a blog post');
